f42d17366613448761613f10cf907975f733ee95
ᴹᵃᵈᵉ ᵒⁿ ᴱᵃʳᵗʰ ᵇʸ ʰᵘᵐᵃⁿˢ <39050604+EXPITC@users.noreply.github.com>
2023-01-20 22:46:15 +0700

    feat(state): pseudo-classes
    + components/button.tsx
    + Hover, focus, and active modifer.
    + Help comperhensive.

--- core-concepts/state/components/button.tsx
+++ core-concepts/state/components/button.tsx
@@ -7,8 +7,7 @@ export default function Button({ value }: Button): JSX.Element {
 
   return (
     <button className="
-      dark:md:hover:bg-fuchsia-600
-      bg-sky-500 hover:bg-sky-700 
+      bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300
       py-2 px-5 text-sm font-medium rounded-full">
       {value}
     </button>
