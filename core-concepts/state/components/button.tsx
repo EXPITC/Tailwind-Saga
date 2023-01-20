245296d46e8988fd5553e9a13e2f0fdcacf4f27f
ᴹᵃᵈᵉ ᵒⁿ ᴱᵃʳᵗʰ ᵇʸ ʰᵘᵐᵃⁿˢ <39050604+EXPITC@users.noreply.github.com>
2023-01-20 22:28:34 +0700

    feat(state):stacked modifers
    + components/button.tsx
    + Trying to apply modifers w tailwind, modifers like pseudo-classes, pseudo-elements, media queries,
      and attribute selector can be stacked in tailwind.
    + Very helping at fast development components.

--- core-concepts/state/components/button.tsx
+++ core-concepts/state/components/button.tsx
@@ -7,6 +7,7 @@ export default function Button({ value }: Button): JSX.Element {
 
   return (
     <button className="
+      dark:md:hover:bg-fuchsia-600
       bg-sky-500 hover:bg-sky-700 
       py-2 px-5 text-sm font-medium rounded-full">
       {value}
