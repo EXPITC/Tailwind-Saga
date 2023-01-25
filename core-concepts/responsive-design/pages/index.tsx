93951fa402fb931fc3bd3c12fea247c50cffdc42
ᴹᵃᵈᵉ ᵒⁿ ᴱᵃʳᵗʰ ᵇʸ ʰᵘᵐᵃⁿˢ <39050604+EXPITC@users.noreply.github.com>
2023-01-25 10:17:00 +0700

    feat(responsive-design): apply component concept
    + next.config.js
      pages/index.tsx
      tailwind.config.js
    + Creating component that apply responsive-design, this component using
      breakprefix from tailwind which using breakpoint name like
      `sm`,`md`,`lg` etc.
    + Not just stop at there it can be use for targeting a break point range
      too just using `max-{breakpoint name}`.
    + This component apply customizing theme too, tailwind allow us to
      mess with the config and make own costum breakpoint in our case.
    + Last but not least in this implementation I use the arbitary values so
      we can see from diffrent angle of break point implementation with
      tailwind.
    + Make responsive easier.

--- core-concepts/responsive-design/pages/index.tsx
+++ core-concepts/responsive-design/pages/index.tsx
@@ -1,5 +1,8 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+
+const imageUrl = "https://images.unsplash.com/photo-1637734433731-621aca1c8cb6"
 
 export default function Home() {
   return (
@@ -10,10 +13,20 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <h1 className="text-orange-500 text-3xl font-bold underline">
-          Hello world!
-        </h1>
+      <main className="flex items-center justify-center p-8 w-screen h-screen">
+        <div className="max-w-md sm:max-w-2xl mx-auto bg-white rounded-xl shadow-md overflow-hidden ring-1 ring-slate-900/5">
+          <div className="flex flex-col min-[640px]:flex-row">
+            <div className="tablet:shrink-auto">
+              <Image className="w-full sm:w-[192px] h-[200px] object-cover object-bottom sm:object-center"
+                src={imageUrl} priority width={4000} height={5328} alt="Modern building architecture" />
+            </div>
+            <div className="p-6 2xl:p-8 space-y-2">
+              <div className="uppercase tracking-wide text-sm text-indigo-500 font-medium leading-6">Company retreats</div>
+              <a href="#" className="block text-base text-slate-900 font-semibold leading-6 hover:underline">Incredible accommondation for your team</a>
+              <p className="text-sm text-slate-600 leading-6">Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.</p>
+            </div>
+          </div>
+        </div>
       </main>
     </>
   )
--- core-concepts/responsive-design/tailwind.config.js
+++ core-concepts/responsive-design/tailwind.config.js
@@ -6,6 +6,23 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      // default
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      // If i dont provide default value above costume screen name will overide default above
+      // I put it for educational purpose
+
+      'tablet': '640px',
+      // => @media (min-width: 640px) {...}
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) {...}
+      'desktop': '1280px'
+      // => @media (min-width: 1280px) {...}
+    }
   },
   plugins: [],
 }
