934654cadf8338cf038900f33d7556f76661f88e
ᴹᵃᵈᵉ ᵒⁿ ᴱᵃʳᵗʰ ᵇʸ ʰᵘᵐᵃⁿˢ <39050604+EXPITC@users.noreply.github.com>
2023-01-25 11:03:53 +0700

    fix(responsive-design): Image small while wide
    + pages/index.tsx
    + `flex-shrink: auto` cause behavior Image to small when in wide screen,
      change to `flex-shrink:0`.
    + Image behavior should be as expected when in wide or `min-640px` screen.

--- core-concepts/responsive-design/pages/index.tsx
+++ core-concepts/responsive-design/pages/index.tsx
@@ -14,14 +14,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex items-center justify-center p-8 w-screen h-screen">
-        <div className="max-w-md sm:max-w-2xl mx-auto bg-white rounded-xl shadow-md overflow-hidden ring-1 ring-slate-900/5">
+        <div className="max-w-md sm:max-w-2xl mx-auto bg-white rounded-lg shadow-xl overflow-hidden ring-1 ring-slate-900/5">
           <div className="flex flex-col min-[640px]:flex-row">
-            <div className="tablet:shrink-auto">
-              <Image className="w-full sm:w-[192px] h-[200px] object-cover object-bottom sm:object-center"
-                src={imageUrl} priority width={4000} height={5328} alt="Modern building architecture" />
-            </div>
+            <div className="tablet:shrink-0">
+              <Image className="w-full sm:w-[193px] h-[200px] object-cover object-bottom sm:object-center"
+                src={imageUrl} priority width={202} height={192} alt="Modern building architecture" /> </div>
             <div className="p-6 2xl:p-8 space-y-2">
-              <div className="uppercase tracking-wide text-sm text-indigo-500 font-medium leading-6">Company retreats</div>
+              <div className="text-sm text-indigo-600 font-medium leading-6">Company retreats</div>
               <a href="#" className="block text-base text-slate-900 font-semibold leading-6 hover:underline">Incredible accommondation for your team</a>
               <p className="text-sm text-slate-600 leading-6">Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.</p>
             </div>
