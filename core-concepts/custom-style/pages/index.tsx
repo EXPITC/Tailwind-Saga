5ba0261ed333660b48ecb5c71d4916a6ab6ff10d
ᴹᵃᵈᵉ ᵒⁿ ᴱᵃʳᵗʰ ᵇʸ ʰᵘᵐᵃⁿˢ <39050604+EXPITC@users.noreply.github.com>
2023-01-27 16:02:28 +0700

    feat(custom-style): arbitrary properties
    + pages/index.tsx
    + Create skull mask svg with arbitrary properties.
    + If you ever need to use CSS property that Tailwind doesn't included
      a utility for out of the box, you can also use square bracket
      notation to write completely arbitrary CSS and it can also combine
      with iteractive modifier to.

--- core-concepts/custom-style/pages/index.tsx
+++ core-concepts/custom-style/pages/index.tsx
@@ -10,12 +10,19 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main className="w-screen h-screen bg-pink flex justify-center">
         <h1 className="absolute top-[117px] lg:top-[344px]
           bg-[#bada55] text-[22px] before:content-['Festivus'] 
           text-gray-dark font-sans text-3xl font-bold underline">
           {' '}Hello world!
         </h1>
+        <div className="
+          [mask-image:url('https://api.iconify.design/whh:skull.svg')] 
+          [mask-repeat:no-repeat]
+          [mask-size:contain]
+          bg-gradient-to-r from-orange to-pink
+          h-80 w-80">
+        </div>
       </main>
     </>
   )
